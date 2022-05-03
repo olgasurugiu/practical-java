192e7d655d1bfaee4699d589bb136a799e015485
olgasurugiu <olga_surugiu@yahoo.com>
2022-05-03 14:10:39 +0300

    Add elasticsearch datasource
    Add 10000 dummy car data to elasticsearch

--- src/main/java/com/course/practicaljava/entity/Car.java
+++ src/main/java/com/course/practicaljava/entity/Car.java
@@ -3,6 +3,12 @@ package com.course.practicaljava.entity;
 import java.time.LocalDate;
 import java.util.List;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.DateFormat;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
@@ -10,6 +16,7 @@ import com.fasterxml.jackson.annotation.JsonUnwrapped;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
+@Document(indexName="practical-java")
 @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 public class Car {
 
@@ -17,43 +24,47 @@ public class Car {
 	private List<String> additionalFeatures;
 	
 	private boolean available;
-	
-	private String brand;
-	
-	private String color;
 
+	private String brand;
+
+	private String color;
+	
 	@JsonUnwrapped
 	private Engine engine;
-
-	@JsonFormat(pattern = "dd-MMM-yyyy", timezone = "Asia/Jakarta")
+	
+	@Field(type = FieldType.Date, format = DateFormat.date)
+	@JsonFormat(pattern = "yyyy-MMM-dd", timezone = "Asia/Jakarta")
 	private LocalDate firstReleaseDate;
 	
+	@Id
+	private String id;
+
 	private int price;
-	
+
 	@JsonInclude(value = Include.NON_EMPTY)
 	private String secretFeature;
-
-	private List<Tire> tires;
-
-	private String type;
 	
+	private List<Tire> tires;
+	
+	private String type;
+
 	public Car() {
 	}
-	
+
 	public Car(String brand, String color, String type) {
 		super();
 		this.brand = brand;
 		this.color = color;
 		this.type = type;
-	}	
-
+	}
+	
 	public List<String> getAdditionalFeatures() {
 		return additionalFeatures;
 	}
-
+	
 	public String getBrand() {
 		return brand;
-	}
+	}	
 
 	public String getColor() {
 		return color;
@@ -65,6 +76,10 @@ public class Car {
 
 	public LocalDate getFirstReleaseDate() {
 		return firstReleaseDate;
+	}
+
+	public String getId() {
+		return id;
 	}
 
 	public int getPrice() {
@@ -86,7 +101,7 @@ public class Car {
 	public boolean isAvailable() {
 		return available;
 	}
-	
+
 	public void setAdditionalFeatures(List<String> additionalFeatures) {
 		this.additionalFeatures = additionalFeatures;
 	}
@@ -102,13 +117,17 @@ public class Car {
 	public void setColor(String color) {
 		this.color = color;
 	}
-
+	
 	public void setEngine(Engine engine) {
 		this.engine = engine;
 	}
 
 	public void setFirstReleaseDate(LocalDate firstReleaseDate) {
 		this.firstReleaseDate = firstReleaseDate;
+	}
+
+	public void setId(String id) {
+		this.id = id;
 	}
 
 	public void setPrice(int price) {
